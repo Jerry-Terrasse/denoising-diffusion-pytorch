59cfd3ef2a04a53468f5d6756802e8a5d09e7275
Terrasse <terrasse@qq.com>
2024-01-30 16:58:17 +0800

    add my_train.py & training result gif

--- my_train.py
+++ my_train.py
@@ -32,14 +32,17 @@ diffusion = GaussianDiffusion(
 trainer = Trainer(
     diffusion,
     './cifar10_dog',
-    train_batch_size = 32,
+    train_batch_size = 16,
     train_lr = 8e-5,
-    train_num_steps = 700000,         # total training steps
+    train_num_steps = 210000,         # total training steps
     gradient_accumulate_every = 2,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
     amp = True,                       # turn on mixed precision
-    calculate_fid = True              # whether to calculate fid during training
+    calculate_fid = False,              # whether to calculate fid during training
+    save_and_sample_every=200,
 )
+
+trainer.load(105)
 
 trainer.train()
 
